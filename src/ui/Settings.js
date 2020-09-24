c0c6ba2a36e6e09cced0f6927217ff0076357f9e
Randall Lowe <safelock@users.noreply.github.com>
2020-09-24 14:39:57 -0700

    Update Settings.js

--- src/ui/Settings.js
+++ src/ui/Settings.js
@@ -36,7 +36,7 @@ const Settings = {
   view: function (vnode) {
     document.title = 'Settings'
     return [
-      m('.meta', {content: {width=device-width, initial-scale=1.0}}),
+      m('.meta', {name: {viewport}, content: {width=device-width, initial-scale=1.0}}),
       m('.header', m('h1', 'Settings')),
       m('.settings-section', [
         m('.desc', [
